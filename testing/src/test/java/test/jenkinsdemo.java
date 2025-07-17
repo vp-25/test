37938e7f1bbc12638f714a03a2b67ec00911bbdb
vp-25 <vishnupriyakonadti@gmail.com>
2025-07-17 07:22:24 +0530

    push

---
+++ testing/src/test/java/test/jenkinsdemo.java
@@ -0,0 +1,19 @@
+package test;
+
+public class jenkinsdemo {
+	
+	public static void main(String[]args) {
+		int a=10;
+		int b=20;
+		if(a>b) {
+			System.out.println(" a is greater " +a);
+		}
+		else if (b>a){
+			System.out.println(" b is greater = " +b);
+		}
+		else {
+			System.out.println("good bye ");
+		}
+	}
+
+}
