f17c3bf77eefd3a45b20069c152379d3afc0398e
vp-25 <vishnupriyakonadti@gmail.com>
2025-07-17 08:15:08 +0530

    push

--- testing/src/test/java/test/jenkinsdemo.java
+++ testing/src/test/java/test/jenkinsdemo.java
@@ -1,20 +1,43 @@
 package test;
 
-public class jenkinsdemo {
-	//hiii triggerr
-	//hello
-	public static void main(String[]args) {
-		int a=10;
-		int b=20;
-		if(a>b) {
-			System.out.println(" a is greater " +a);
-		}
-		else if (b>a){
-			System.out.println(" b is greater = " +b);
-		}
-		else {
-			System.out.println("good bye ");
-		}
-	}
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 
+public class jenkinsdemo {
+
+    static WebDriver driver;
+    static WebDriver sdriver;
+
+    public static void main(String[] args) {
+
+        String BROWSER = System.getProperty("browser");
+
+        if (BROWSER.equalsIgnoreCase("chrome")) {
+            driver = new ChromeDriver();
+        } else if (BROWSER.equalsIgnoreCase("firefox")) {
+            driver = new FirefoxDriver();
+        } else if (BROWSER.equalsIgnoreCase("edge")) {
+            driver = new EdgeDriver();
+        } else {
+            System.out.println("Invalid browser passed: " + BROWSER);
+            return;
+        }
+
+        System.out.println("Browser Launching...");
+        sdriver = driver;
+
+        int a = 10;
+        int b = 20;
+        if (a > b) {
+            System.out.println("a is greater = " + a);
+        } else if (b > a) {
+            System.out.println("b is greater = " + b);
+        } else {
+            System.out.println("good bye");
+        }
+
+        driver.quit();
+    }
 }
